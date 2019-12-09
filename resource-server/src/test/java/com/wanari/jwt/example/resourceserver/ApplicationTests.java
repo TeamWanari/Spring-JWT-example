2b7fbe5374bcf75b702a6cb9bd5e4d81824d23de
Sükein Alex <suxy@wanari.com>
2019-12-09 15:08:24 +0100

    [resource-server] initialize project

---
+++ resource-server/src/test/java/com/wanari/jwt/example/resourceserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanari.jwt.example.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
