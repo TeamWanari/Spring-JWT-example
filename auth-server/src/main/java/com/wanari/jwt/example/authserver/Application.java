cff6c3e31a8b766afbf35b1040fb755fcf49f4d6
Sükein Alex <suxy@wanari.com>
2019-12-09 12:30:00 +0100

    [auth-server] initialize project

---
+++ auth-server/src/main/java/com/wanari/jwt/example/authserver/Application.java
@@ -0,0 +1,13 @@
+package com.wanari.jwt.example.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
